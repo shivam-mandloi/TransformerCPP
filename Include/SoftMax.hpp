cddf3d900b3187b62d15260813f2203e72c04583
Shivam Mandloi <shivammandloi1102@gmail.com>
2025-10-19 12:28:17 +0530

    add softmax gradient

--- Include/SoftMax.hpp
+++ Include/SoftMax.hpp
@@ -3,6 +3,12 @@
 #include "LinAlg.hpp"
 #include <functional>
 
+/*
+    forward -> Input <column vector> / return <column vector>
+
+    backward -> Input <column vector> / return <column vector>
+*/
+
 std::function<double(double, double, double)> clip = [](double minVal, double maxVal, double val){
     return val > maxVal ? maxVal : (minVal > val ? minVal : val);
 };
@@ -22,13 +28,32 @@ public:
         }
         for(int i = 0; i < input.len; i++)
             input.push(i, input.Get(i)/total);
+        prob = input;
     }
 
     void backward(vecX<double> &prevGrad)
     {
+        // we assume that prevGrad is column vector
+        vecX<double> derMat(prevGrad.len, prevGrad.len, 0);
         
+        // grad dy/dx
+        for(int i = 0; i < prevGrad.len; i++)
+        {
+            for(int j = 0; j < prevGrad.len; j++)
+            {
+                if(i == j)
+                {
+                    derMat.push(i, j, prob.Get(i) * (1- prob.Get(i)));
+                    continue;
+                }
+                derMat.push(i, j, - prob.Get(i) * prob.Get(j));
+            }
+        }
+        prevGrad.TR();
+        prevGrad = MatMul(prevGrad, derMat);
     }
 
 private:
     double temprature;
+    vecX<double> prob;
 };
